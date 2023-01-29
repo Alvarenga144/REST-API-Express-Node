e2a56a0b6e7633ef9af53ca1eea5e762466ee536
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2023-01-29 00:28:33 -0600

    Añadiendo Rutas - Routing

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Ruta por defecto
 app.get('/', (req, res) => {
   res.send('Mi primer servidor con Express');
 });
+// Ruta ejemplo 1
+app.get('/home', (req, res) => {
+  res.send('Estamos en Home');
+});
+// Ruta ejemplo 2
+app.get('/products', (req, res) => {
+  res.send('Estamos en Products');
+});
+// Ruta retornando json
+app.get('/json', (req, res) => {
+  res.json({
+    name: "Esteban",
+    lastname: "Alvarenga",
+    age: 20,
+    profession: "Developer",
+    address: "San Salvador, SV"
+  });
+});
 
+// Impresion en consola al levantar el puerto.
 app.listen(port, () => {
   console.log('Mi puerto: ' + port)
 });
