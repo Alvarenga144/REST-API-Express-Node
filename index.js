1e0b1f10c2cf024a90626a8fbf9866fadc95d045
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2023-01-29 00:57:25 -0600

    GET: Recibir parametros

--- index.js
+++ index.js
@@ -14,16 +14,47 @@ app.get('/home', (req, res) => {
 app.get('/products', (req, res) => {
   res.send('Estamos en Products');
 });
-// Ruta retornando json
-app.get('/json', (req, res) => {
-  res.json({
+// Ruta retornando array de objetos
+app.get('/people', (req, res) => {
+  res.json([{
     name: "Esteban",
     lastname: "Alvarenga",
     age: 20,
     profession: "Developer",
-    address: "San Salvador, SV"
-  });
+    address: "San Salvador, SV",
+  },
+  {
+    name: "Andrea",
+    lastname: "Alfaro",
+    age: 18,
+    profession: "Designer",
+    address: "Chalatenango, SV",
+  },
+  ]);
 });
+
+app.get('/people/:id_people', (req, res) => {
+  const { id_people } = req.params;
+  res.json( {
+    id_people,
+    name: "Andrea",
+    lastname: "Alfaro",
+    age: 18,
+    profession: "Designer",
+    address: "Chalatenango, SV",
+  })
+});
+
+app.get('/home/:id_myhome/people/:id_people', (req, res) => {
+  const { id_myhome ,id_people } = req.params;
+  res.json({
+    id_myhome,
+    id_people,
+  })
+});
+
+
+
 
 // Impresion en consola al levantar el puerto.
 app.listen(port, () => {
